6154da6f15c85428de48d24b6a2913becfdb497f
Mostafa <112512917+mostafa587@users.noreply.github.com>
2024-12-31 16:13:55 +0200

    Add files via upload

--- My_Resume/settings.py
+++ My_Resume/settings.py
@@ -38,10 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-
     # My apps
     'portfolio',
 ]
+
+# SASS Processor settings
+SASS_PROCESSOR_ROOT = BASE_DIR / 'portfolio/static'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -119,7 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "portfolio/static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
