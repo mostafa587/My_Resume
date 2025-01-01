e051da55511e4163945be6d21db910a5d86b3da3
Mostafa <112512917+mostafa587@users.noreply.github.com>
2025-01-01 14:30:38 +0200

    Update settings.py

--- My_Resume/settings.py
+++ My_Resume/settings.py
@@ -19,11 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g_tm)9@ufz92e35&f0#i&n9=g@1#b^m6l6h%z=%7!qmuhhlzwa'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
 ALLOWED_HOSTS = []
 
